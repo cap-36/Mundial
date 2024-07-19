c071f0789c2183c430d2063bbc5b173578564b52
Camilo Pinzon <82333560+cap-36@users.noreply.github.com>
2024-07-18 21:43:24 -0500

    Delete php directory

--- php/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$conexion = new PDO("pgsql:host=172.27.100.99 dbname=bdfutbol user=postgres port=5432 password=postgres04");
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-
-    // echo json_encode('conectado correctamente');
